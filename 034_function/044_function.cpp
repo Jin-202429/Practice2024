66895d8375b3f2f94c3c9855045bf3543346d82d
Jin-202429 <ncheeunho@gmail.com>
2024-12-09 23:33:10 +0900

    Add files via upload

---
+++ 034_function/044_function.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+double plus(double a, double b) {
+	return a + b;
+}
+
+double minus(double a, double b) {
+	return a - b;
+}
+ 
+double multiply(double a, double b) {
+	return a * b;
+}
+
+double divide(double a, double b) {
+	return a / b;
+}
+
+int main()
+{
+	double a, b;
+
+	printf("두 숫자를 입력하세요 : ");
+	scanf_s("%1f %1f", &a, &b);
+
+	printf("더하기 = %.1lf\n", plus(a, b));
+	printf("빼기 = %.1lf\n", minus(a, b));
+	printf("곱하기 = %.1lf\n", multiply(a, b));
+	printf("나누기 = %.1lf\n", divide(a, b));
+}
---
+++ 046_recursive/046_recursive.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+
+/*
+void rec(int n) {
+	if (n > 5)
+		return;
+	printf("n = %d\n", n);
+	rec(n + 1);
+}
+
+int main()
+{
+	rec(1);
+}
+*/
+
+long long fact(long long n)
+{
+	if(n == 1)
+		return 1;
+	return fact (n - 1) * n;
+}
+
+int main()
+{
+	//반복문
+	long long f = 1;  // 곱해나가는 수이므로 1로 초기화 (더해나간다면 0으로 초기화)
+	for (int i = 1; i <= 20; i++) {  // 20팩토리얼 까지만 계산가능
+		f = 1;
+		for (int j = 1; j <= i; j++)
+			f *= j;
+		printf("%d! = %11d\n", i, f);
+	}
+
+	//재귀함수를 사용
+	for (int i = 1; i <= 20; i++)
+		printf("%d! = %11d\n", i, fact(i));
+}
